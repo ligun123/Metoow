38a240c4ed0f4e472fff25e59ea69080ce8efedd
HalloWorld <it@ITtekiiMac.local>
2014-05-08 14:17:26 +0800

    add rich test

---
+++ Metoow/TQRichTextView/TQViewController.h
@@ -0,0 +1,14 @@
+//
+//  TQViewController.h
+//  TQRichTextViewDemo
+//
+//  Created by fuqiang on 2/26/14.
+//  Copyright (c) 2014 fuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TQRichTextView.h"
+
+@interface TQViewController : UIViewController <TQRichTextViewDelegate>
+
+@end
